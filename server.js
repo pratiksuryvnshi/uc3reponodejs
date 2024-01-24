0bde331869c28457e85809ddbfbd11e740a390c2
mayankgg2511 <mayankgg2511@gmail.com>
2024-01-24 20:37:02 +0000

    serverjs change

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello DevOps Cloud World  Training !!! . Welcome To DevOps session. Thank You.');
+  res.send('Hello DevOps Cloud World  Training !!! . Welcome To DevOps session. This is our last class.');
 });
 
 app.listen(PORT, HOST);
