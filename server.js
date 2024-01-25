4e42a3d4870f7ccb44f2af5d9060f5d0c3afb82d
mayankgg2511 <147944182+mayankgg2511@users.noreply.github.com>
2024-01-25 12:38:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello DevOps Cloud World  Training !!! . Welcome To DevOps session. final approch is good only profit no loss at all');
+  res.send('Hello DevOps Cloud World  Training !!! . Welcome To DevOps session. final approch is good welcome abroad');
 });
 
 app.listen(PORT, HOST);
