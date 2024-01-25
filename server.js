2c307ffa5fff90459fec81f6dc0451f7953a287d
mayankgg2511 <mayankgg2511@gmail.com>
2024-01-25 06:17:32 +0000

    first commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello DevOps Cloud World  Training !!! . Welcome To DevOps session. This is our last class mayank repo final one.');
+  res.send('Hello DevOps Cloud World  Training !!! . Welcome To DevOps session. only comng');
 });
 
 app.listen(PORT, HOST);
