e9e5ac076b27a7d84946b1b2b4302f6c44c2bde3
EC2 Default User <ec2-user@ip-172-31-0-231.ap-south-1.compute.internal>
2024-06-02 06:18:25 +0000

    final

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello DevOps Cloud World  Training !!! . Welcome To DevOps session. final approch is good welcome');});
+  res.send('batch817..............Hello DevOps Cloud World  Training !!! . Welcome To DevOps session. final approch is good welcome');});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
